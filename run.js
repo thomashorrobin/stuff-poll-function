1edc0a504fae687deff8cb81592caae057139956
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2017-09-08 16:10:57 +0100

    removed useless code from run.js

--- run.js
+++ run.js
@@ -17,9 +17,3 @@ for (var i = 0; i < 5000; i++) {
         sendRequest('Jacinda Ardern', '87e984df-9706-4930-b95c-e78f3088482d');
     }, timeToDeplopment);
 }
-
-console.log(createGuid());
-console.log(createRandomTimestamp());
-console.log(createRandomIPAddress());
-console.log(createRandomlyDistributedDeploymentTime());
-// sendRequest('Bill English', '862553db-53d2-4301-aa59-87f3c2309724');
